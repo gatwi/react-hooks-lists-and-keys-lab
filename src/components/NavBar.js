9ef82c10f5c9086179c64047ab8a923afbd5dd39
gatwi <faith.murithi@student.moringaschool.com>
2022-07-13 16:13:31 +0300

    <a> and <span> tags created successfully

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,11 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav id="NavBar">
+    <a href="#home">home</a>
+    <a href="#about">about</a>
+    <a href="#projects">projects</a>
+    </nav>;
 }
 
 export default NavBar;
